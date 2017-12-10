5751fdc35b63e9e1c555a582f7c345ac41e74cde
yorraksahb <bhaskarroy@Roys-MacBook-Pro.local>
2017-12-10 14:07:36 +0530

    modifyong logic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var log4js = require('log4js');
+var log4js = require('log4js'),
+    _ = require('underscore');
 
 var printLogs = function() {
-    console.log('Start');
 
     log4js.configure({
         levels: {
@@ -12,22 +12,39 @@ var printLogs = function() {
                 type: 'console',
                 layout: {
                     type: 'pattern',
-                    pattern: '[%r] [%p] [%c] {%x{body}}%n',
+                    pattern: '%[[%d{yyyy-mm-dd hh:mm:ss.SSS}] %p %c (%x{body}) %] %n',
                     tokens: {
-                        body: function() { return process.pid; }
+                        body: function() {
+                            var msg = arguments[0].data[0];
+                            // console.log(msg);
+                            return maskSensitiveData(msg);
+                        }
                     }
                 }
             }
         },
-        categories: { default: { appenders: ['IPM'], level: 'DEBUG' } }
+        categories: { default: { appenders: ['IPM'], level: 'ERROR' } }
     });
 
+    var maskSensitiveData = function(msg) {
+        console.log(_.keys(msg));
+        var foo = msg[_.keys(msg)];
+        var value = foo[_.keys(foo)];
+        console.log(value);
+        return msg;
+    };
+
+    var object1 = {
+        event: {
+            token: 'asdjhdjhsjdhjshdj'
+        }
+    };
     const logger = log4js.getLogger('IPM');
     logger.debug('Debug');
     logger.info('One');
     logger.notice('Notice');
     logger.warn('Warn');
-    logger.error('Error');
-    logger.fatal('Fatal');
+    logger.error(object1);
+    // logger.fatal('Fatal');
 }
 printLogs();
